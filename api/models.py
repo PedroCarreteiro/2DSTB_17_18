11435388075741d179412cc4be3938faed5a9e64
PedroCarreteiro <pcarreteiro01@gmail.com>
2025-08-07 15:17:15 -0300

    Aula

--- api/models.py
+++ api/models.py
@@ -1,5 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Autor(models.Model):
+    nome = models.CharField(max_length=255)
+    sobrenome = models.CharField(max_length=255)
+    data_nasc = models.DateField(null=True, blank=True)
+    nacion = models.CharField(max_length=30, null=True, blank=True)
+    biogra = models.TextField(null=True, blank=True)
+
 
 
