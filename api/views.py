a53a2acc4c7aeaf2723bdc841c316e0b9b78a988
PedroCarreteiro <pcarreteiro01@gmail.com>
2025-08-08 16:08:40 -0300

    Aula

--- api/views.py
+++ api/views.py
@@ -3,6 +3,7 @@ from rest_framework.generics import ListCreateAPIView
 from .models import Autor
 from .serializers import AutorSerializers
 
+#Post
 class AutoresView(ListCreateAPIView):
     queryset = Autor.objects.all()
     serializer_class = AutorSerializers
