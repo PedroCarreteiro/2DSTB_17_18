21938796b92d5fe2697444f89073178f7caf84b1
PedroCarreteiro <pcarreteiro01@gmail.com>
2025-08-08 16:26:50 -0300

    CRUD

--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('autores/', AutoresView.as_view())
+    path('autores/', AutoresView.as_view()),
+    path('autores/<int:pk>', AutorRetrieveUpdateDestroy.as_view())
 ]
--- api/views.py
+++ api/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
-from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from .models import Autor
 from .serializers import AutorSerializers
 
 #Post
 class AutoresView(ListCreateAPIView):
+    queryset = Autor.objects.all()
+    serializer_class = AutorSerializers
+
+class AutorRetrieveUpdateDestroy(RetrieveUpdateDestroyAPIView):
     queryset = Autor.objects.all()
     serializer_class = AutorSerializers
     
